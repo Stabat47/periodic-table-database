f3043738322529f96d01b363eba41c698893668a
Landoh Clansy <clensylandoh@gmail.com>
2025-06-01 04:59:41 +0000

    feat: add argument validation to element.sh

--- element.sh
+++ element.sh
@@ -15,7 +15,7 @@ if [[ $INPUT =~ ^[0-9]+$ ]]
 then
   ELEMENT_DATA=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.atomic_number = $INPUT;")
 else
-  # Otherwise search by symbol or name (case-insensitive)
+  
   ELEMENT_DATA=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE LOWER(e.symbol) = LOWER('$INPUT') OR LOWER(e.name) = LOWER('$INPUT');")
 fi
 
