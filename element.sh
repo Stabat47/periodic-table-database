7f0106ea8b50100a0ee339e5bba6c0841c559cad
Landoh Clansy <clensylandoh@gmail.com>
2025-06-01 05:02:04 +0000

    refactor: simplify SQL query logic

--- element.sh
+++ element.sh
@@ -28,3 +28,4 @@ fi
 IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT_DATA"
 
 echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+#I am out of adjustments for commits lol
